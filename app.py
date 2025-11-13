4592bd5521f1edb549b60c0a21f905fe118fd7dd
kalkyros <darkpoison20000@gmail.com>
2025-11-13 02:41:33 -0300

    acho que agora vai

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG") == "True")
+    debug_mode = os.getenv("FLASK_DEBUG") == "True"
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode, use_reloader=False)
